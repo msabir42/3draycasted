2a94f26e3709e1528be910b788a1ea5625ae28e9
Mohamed Sabir <msabir@e2r8p2.1337.ma>
2025-11-15 11:50:29 +0100

    implementing textures

---
+++ textures.c
@@ -0,0 +1,22 @@
+#include "cub3d.h"
+
+
+int load_textures(t_game* game, t_texture* tex, char* path)
+{
+    if(!game || !tex || !path)
+        return 0;
+    
+    return 1;
+}
+
+void init_textures(t_game* game)
+{
+    (void)game;
+}
+
+void free_textures(t_game* game)
+{
+    (void)game;
+}
+
+
