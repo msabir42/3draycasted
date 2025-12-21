d94f38a150ebaccaabe3fa7b57ba13d278e39253
Mohamed Sabir <msabir@c3r1p2.1337.ma>
2025-12-21 02:49:20 +0100

    leaks left to handle probably

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: msabir <msabir@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/20 17:36:02 by msabir            #+#    #+#             */
-/*   Updated: 2025/12/20 21:25:46 by msabir           ###   ########.fr       */
+/*   Updated: 2025/12/21 02:32:00 by msabir           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,34 +44,32 @@ static void	exit_error(t_game *game, char *msg)
 	exit(1);
 }
 
-int	main(int argc, char **argv)
+static void	run_game(char *map_path)
 {
-	t_game *game;
-
-	if (argc != 2)
-		exit_error(NULL, "Usage: ./cub3D map.cub");
+	t_game	*game;
 
 	game = malloc(sizeof(t_game));
 	if (!game)
-		exit_error(NULL, "Malloc failed");
-
+		exit_error(NULL, "Malloc failed\n");
 	ft_bzero(game, sizeof(t_game));
 	init_data(&game->data);
-
-	if (!fetch_lines(argv[1], &game->data))
+	if (!fetch_lines(map_path, &game->data))
 		exit_error(game, "Failed to parse map\n");
-
 	init_player(&game->player, &game->data);
 	init_game(game, game->data, game->player);
-
 	if (!load_all_textures(game))
 		exit_error(game, "Failed to load textures\n");
-
 	mlx_calls(game);
-
 	cleanup_mlx(game);
 	free_textures(game);
 	cleanup_data(&game->data);
 	free(game);
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc != 2)
+		exit_error(NULL, "Usage: ./cub3D map.cub\n");
+	run_game(argv[1]);
 	return (0);
 }
--- parsing/utils_gnl.c
+++ parsing/utils_gnl.c
@@ -6,67 +6,75 @@
 /*   By: msabir <msabir@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/19 03:44:02 by msabir            #+#    #+#             */
-/*   Updated: 2025/12/20 21:31:05 by msabir           ###   ########.fr       */
+/*   Updated: 2025/12/21 02:25:04 by msabir           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cub3d.h"
 
-static int	append_char(char **line, char c, size_t *len, size_t *cap)
+static int	gnl_grow(char **line, size_t *cap, size_t len)
 {
 	char	*tmp;
 	size_t	i;
-	size_t	new_cap;
 
+	tmp = malloc(*cap * 2);
+	if (!tmp)
+		return (0);
+	i = 0;
+	while (i < len)
+	{
+		tmp[i] = (*line)[i];
+		i++;
+	}
+	free(*line);
+	*line = tmp;
+	*cap *= 2;
+	return (1);
+}
+
+static int	gnl_push(char **line, char c, size_t *len, size_t *cap)
+{
 	if (*len + 1 >= *cap)
 	{
-		new_cap = *cap * 2;
-		tmp = malloc(new_cap);
-		if (!tmp)
-		{
-			free(*line);
-			*line = NULL;
+		if (!gnl_grow(line, cap, *len))
 			return (0);
-		}
-		i = 0;
-		while (i < *len)
-		{
-			tmp[i] = (*line)[i];
-			i++;
-		}
-		free(*line);
-		*line = tmp;
-		*cap = new_cap;
 	}
-	(*line)[(*len)++] = c;
+	(*line)[*len] = c;
+	(*len)++;
 	return (1);
+}
+
+static int	gnl_read(int fd, char **line, size_t *len, size_t *cap)
+{
+	char	buffer[1];
+	ssize_t	r;
+
+	r = read(fd, buffer, 1);
+	while (r > 0)
+	{
+		if (!gnl_push(line, buffer[0], len, cap))
+			return (-1);
+		if (buffer[0] == '\n')
+			break ;
+		r = read(fd, buffer, 1);
+	}
+	return (r);
 }
 
 char	*get_next_line(int fd)
 {
-	char	buffer[1];
 	char	*line;
 	size_t	len;
 	size_t	cap;
-	ssize_t	bytes_read;
+	int		r;
 
 	cap = 128;
 	len = 0;
 	line = malloc(cap);
 	if (!line)
 		return (NULL);
-	bytes_read = read(fd, buffer, 1);
-	while (bytes_read > 0)
-	{
-		if (!append_char(&line, buffer[0], &len, &cap))
-		{
-			return (NULL);
-		}
-		if (buffer[0] == '\n')
-			break ;
-		bytes_read = read(fd, buffer, 1);
-	}
-	if (bytes_read < 0 || len == 0)
+	r = gnl_read(fd, &line, &len, &cap);
+	if (r < 0 || len == 0)
 	{
 		free(line);
 		return (NULL);
